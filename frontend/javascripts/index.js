56612b37adbc8b5081816b09275188617abaeeb6
Adriana <am20dipi@alum.siena.edu>
2021-06-10 17:51:13 -0400

    Working on both renderTask and createTask functions, trying to debug

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -10,12 +10,16 @@ const editTaskButton = () => document.getElementById("edit-task")
 
 //Lists
 const listDiv = () => document.getElementById("list")
-const ulTaskList = () => document.getElementById("task-list")   
+const ulTaskList = () => document.getElementById("task-list")
+const oneTaskDiv = () => document.getElementById("show-one-task")
+const ulOneTask = () => document.getElementById("one-task")      
 
 //Forms
 const newTaskForm = () => document.querySelector("#new-task-form")
 const formContainer = () => document.querySelector("#form-container")
-
+const taskName = () => document.getElementById("#task-name")
+const taskDueDate = () => document.getElementById("#task-due-date")
+const taskCompleted = () => document.getElementById("#task-completed")
 
 // 2. Events
     // DOMContentLoaded => when the DOM finishes loading, listen for the specified events 
@@ -28,24 +32,24 @@ document.addEventListener("DOMContentLoaded", () => {
     addTaskButton().addEventListener("click", displayNewForm)
 })
 
-
+// 3. Functions
 
 const displayNewForm = () => {
     // 1. HTML to show the form
     // 2. insert onto page?
     // 3. submit button / event listener
-    debugger
+    //debugger
     const form = document.querySelector("#new-task-form")
     form.innerHTML = `
-    <label for="name">Task:</label>
-    <input type="text" id="name" name="name"><br>
-    <label for="due_date">Due Date:</label>
-    <input type="text" id="due_date" name="due_date"><br>
-    <label for="completed">Completed?:</label>
-    <label>Yes</label><input type="checkbox" id="done" name="completed" value ="done">
-    <label>No</label><input type="checkbox" id="not done" name="completed" value="not done"><br>
-    <label for="task_notes">Notes:</label>
-    <textarea id="task_notes" name="task_notes"></textarea><br><br>
+    
+    Name: <input type="text" id="task-name" name="name"><br>
+    
+    Due Date:<input type="text" id="task-due_date" name="due_date"><br>
+    
+    Completed:<label>Yes</label><input type="checkbox" id="task-completed" name="completed" value ="done">
+    <label>No</label><input type="checkbox" id="task-completed" name="completed" value="not done"><br>
+    
+    Notes:<textarea id="task_notes" name="task_notes"></textarea><br><br>
     <input type="submit" value="Create Task">
     `
     formContainer().appendChild(form)
@@ -53,17 +57,20 @@ const displayNewForm = () => {
 
 }
 
-const handleSubmit = () => {
+const handleSubmit = (e) => {
     // 1. prevent default
     // 2. fetch to endpoint, POST, body, headers
     // 3. create the new task 
     // 4. send data
+    //debugger
     e.preventDefault()
     const formData = {
         name: taskName().value,
         due_date: taskDueDate().value,
         completed: taskCompleted().value
+
     }
+    
      fetch('http://localhost:3000/tasks', {
          method: "POST",
          headers: {
@@ -75,9 +82,28 @@ const handleSubmit = () => {
      .then(resp =>resp(json))
      .then(json => createTask(json))
      .catch(handleError)
+     debugger
     
 }
 
+const createTask = (task) => {
+    // 1. takes in task as argument
+    // 2. new task must be on the page (renderTask)
+    debugger
+    renderTask(task)
+
+}
+
+const renderTask = (task) => {
+    const li = document.createElement("li")
+        li.innerHTML = `
+            <h2 class="task-name">${task.name}</h2>
+            <p class="task-duedate">${task.due_date}</p>
+            <p class="task-completed">${task.completed}</p>
+        `
+        ulOneTask().appendChild(li)
+}
+ 
 
 
 const handleClick = () => {
