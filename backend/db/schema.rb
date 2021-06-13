fc3c52d48c8ba4c3ab00b494ffbce454da96eea7
Adriana <am20dipi@alum.siena.edu>
2021-06-13 15:10:30 -0400

    Generating and migrating category scaffold, generating belongs_to association through table column

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_184751) do
+ActiveRecord::Schema.define(version: 2021_06_13_190940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
